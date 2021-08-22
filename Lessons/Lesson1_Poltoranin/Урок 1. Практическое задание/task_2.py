2676cb8988f1ce14d63d840efa5325d08b3bca10
nankuF <fireloki87@gmail.com>
2021-08-22 21:19:39 +0300

    dz1

--- Lessons/Lesson1_Poltoranin/Урок 1. Практическое задание/task_2.py
+++ Lessons/Lesson1_Poltoranin/Урок 1. Практическое задание/task_2.py
@@ -10,3 +10,42 @@
 --- b'class' - используйте маркировку b''
 --- используйте списки и циклы, не дублируйте функции
 """
+
+s1 = b'class'
+s2 = b'function'
+s3 = b'method'
+print(s1, '|', type(s1), '|', 'len:', len(s1))
+print(s2, '|', type(s2), '|', 'len:', len(s2))
+print(s3, '|', type(s3), '|', 'len:', len(s3))
+
+
+def bin_format(*args: str) -> []:
+    lst = [*args]
+    lst_b_string = []
+    for i in lst:
+        lst_b_string.append(bytes(i, 'utf-8'))
+
+    bin_s = ''
+    bin_lst = []
+    for i, el in enumerate(lst_b_string):
+        for els in el:
+            bin_s += bin(els)
+        bin_s = bin_s.replace('b', '')  # необязательно удалять, можно и так chr(int('0b1100011',2) == 'c'
+        bin_lst.append((bin_s, lst[i]))
+        bin_s = ''
+
+    return bin_lst
+
+
+result = bin_format('class', 'function', 'method')
+# print('Рез-т работы ф-ии: ', result)
+for i in result:
+    print(type(i[0]), i[0], '| Длина:', len(i[0]), '| Слово:', i[1])
+
+# c = b'c'  # указываем что строка байтовая
+# b = ''
+# s = ''
+# for el in c:
+#     b += bin(el)  # преобразуем байтовую строку в бинарный формат
+#     s += chr(int(b, 2))  # преобразуем бинарный формат в буквы (привычные символы)
+# print(b, s)
--- Lessons/Lesson1_Poltoranin/Урок 1. Практическое задание/task_4.py
+++ Lessons/Lesson1_Poltoranin/Урок 1. Практическое задание/task_4.py
@@ -8,3 +8,15 @@
 Подсказки:
 --- используйте списки и циклы, не дублируйте функции
 """
+
+lst = ['разработка', 'администрирование', 'protocol', 'standard']
+lst_encode = []
+lst_decode = []
+for i in lst:
+    lst_encode.append(i.encode(encoding='utf-8'))
+
+for i in lst_encode:
+    lst_decode.append(i.decode(encoding='utf-8'))
+
+print(lst_encode)
+print(lst_decode)
