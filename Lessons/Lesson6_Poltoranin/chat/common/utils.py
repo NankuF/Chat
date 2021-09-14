e7a9d064a8fd63e6f59d49af4777ffc4bd2be58b
nankuF <fireloki87@gmail.com>
2021-09-14 12:54:03 +0300

    lesson6

---
+++ Lessons/Lesson6_Poltoranin/chat/common/utils.py
@@ -0,0 +1,153 @@
+import json
+from socket import socket
+import sys
+import time
+
+from .variables import DEFAULT_PORT, DEFAULT_ADDRESS, ENCODING, MAX_PACKAGE_LENGTH, ACTION, USER, RESPONSE, PRESENCE, \
+    ACCOUNT_NAME, TIME, ERROR
+
+import logging
+from Lessons.Lesson6_Poltoranin.chat.decos import log
+
+SERVER_LOGGER = logging.getLogger('server')
+CLIENT_LOGGER = logging.getLogger('client')
+
+@log
+def create_presence(account_name='Guest'):
+    """
+    Функция генерирует ответ о присутствии клиента
+    """
+    # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name
+        }
+    }
+    CLIENT_LOGGER.debug(f'Сформировано {PRESENCE} сообщение для пользователя {account_name}')
+    return out
+
+@log
+def process_ans(message):
+    """
+    Функция разбирает ответ сервера и возвращает 200 либо 400
+    """
+    CLIENT_LOGGER.debug(f'Разбор сообщения от сервера: {message}')
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200 : OK'
+        return f'400 : {message[ERROR]}'
+    raise ValueError
+
+@log
+def process_client_message(message):
+    '''
+    Обработчик сообщений от клиентов, принимает словарь -
+    сообщение от клинта, проверяет корректность,
+    возвращает словарь-ответ для клиента
+    '''
+    SERVER_LOGGER.debug(f'Разбор сообщения от клиента : {message}')
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+        return {RESPONSE: 200}
+    return {
+        RESPONSE: 400,
+        ERROR: 'Bad Request'
+    }
+
+@log
+def console_reader() -> (int, int):
+    """
+    Определяет параметры командной строки, если их нет - задаёт дефолтные значения.
+    Пример командной строки: server.py -p 15000 -a 0.0.0.0
+    """
+
+    def return_port_and_address():
+
+        if 'server.py' in sys.argv and 'client.py' in sys.argv:
+            raise ValueError(print('В функции должен быть только один параметр'))
+
+        else:
+            if 'server.py' in sys.argv:  # server.py -p 15000 -a 0.0.0.0
+                try:
+                    if '-p' in sys.argv:
+                        take_port = sys.argv.index('-p') + 1
+                        port = int(sys.argv[take_port])
+                        if port < 1024 or port > 65535:
+                            SERVER_LOGGER.critical(f'Попытка подключение к серверу с указанием неподходящего порта '
+                                                   f'{port}. Допустимы адреса с 1024 до 65535.')
+                            raise ValueError
+                    else:
+                        port = DEFAULT_PORT
+                    if '-a' in sys.argv:
+                        take_address = sys.argv.index('-a') + 1
+                        address = sys.argv[take_address]
+                    else:
+                        address = DEFAULT_ADDRESS
+                    return address, port
+                except IndexError:
+                    SERVER_LOGGER.error(
+                        f'После параметра -\'p\' необходимо указать номер порта.'
+                        f' В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
+                    sys.exit(1)
+
+            if 'client.py' in sys.argv:  # client.py 192.168.1.2 8079
+                try:
+                    address = sys.argv[1]
+                    port = int(sys.argv[2])
+                    if port < 1024 or port > 65535:
+                        CLIENT_LOGGER.critical(f'Попытка подключение к серверу с указанием неподходящего порта '
+                                               f'{port}. Допустимы адреса с 1024 до 65535.')
+                        raise ValueError
+                except IndexError:
+                    address = DEFAULT_ADDRESS
+                    port = DEFAULT_PORT
+                except ValueError:
+                    CLIENT_LOGGER.critical(
+                        'В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
+                    sys.exit(1)
+                return address, port
+
+        if ('client.py' or 'server.py') not in sys.argv:
+            address = DEFAULT_ADDRESS
+            port = DEFAULT_PORT
+            return address, port
+
+    return return_port_and_address()
+
+@log
+def send_msg(socket: socket, msq: dict) -> None:
+    """
+    Отправляет сообщение, кодирует в байты
+    """
+    if isinstance(msq, dict):
+        json_msg = json.dumps(msq)  # приводим dict к str, чтобы затем ее закодировать
+        encode_msg = json_msg.encode(ENCODING)
+    else:
+        raise TypeError
+
+    if isinstance(encode_msg, bytes):
+        socket.send(encode_msg)
+
+@log
+def recv_msg(socket: socket) -> dict:
+    """
+    Принимает сообщение, декодирует в dict
+    """
+    encoded_response = socket.recv(MAX_PACKAGE_LENGTH)
+    if isinstance(encoded_response, bytes):
+        json_response = encoded_response.decode(ENCODING)
+        response = json.loads(json_response)
+        if isinstance(response, dict):
+            return response
+        raise ValueError
+    raise ValueError
+
+# b = b'"hello"'
+# j_dec = b.decode('utf-8')
+# j_dec = json.loads(j_dec)
+# # print(j_dec, type(j_dec))
+#
+# lst = [0, '-p',2]
+# print(lst.index('-p')+1)
---
+++ Lessons/Lesson6_Poltoranin/chat/server.py
@@ -0,0 +1,53 @@
+import json
+import socket
+from common.utils import send_msg, recv_msg, console_reader, process_client_message
+
+import logging
+import log.server_log_config
+
+
+# Инициализация логирования сервера.
+SERVER_LOGGER = logging.getLogger('server')
+
+
+def main():
+    """
+    Cоздаём сокет-сервер
+    Константа AF_INET соответствует Internet-домену. Сокеты, размещённые в этом домене,
+    могут использоваться для работы в любой IP-сети. AF - address family
+
+    Тип сокета определяет способ передачи данных по сети:
+    SOCK_STREAM. Передача потока данных с предварительной установкой соединения. == TCP
+    SOCK_DGRAM. Передача данных в виде отдельных сообщений (датаграмм). == UDP
+    SOCK_RAW. Этот тип присваивается низкоуровневым (т. н. "сырым") сокетам.
+    Их отличие от обычных сокетов состоит в том, что с их помощью программа может взять на себя
+    формирование некоторых заголовков, добавляемых к сообщению.
+    """
+
+    listen_address, listen_port = console_reader()
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((listen_address, listen_port))
+    sock.listen(2)
+
+    while True:
+        client, client_address = sock.accept()
+        SERVER_LOGGER.info(f'Установлено соединение с ПК {client_address}')
+        try:
+            # получаем от клиента сообщение
+            msg_to_client = recv_msg(client)
+            SERVER_LOGGER.debug(f'Получено сообщение {msg_to_client}')
+            # Проверям, что сообщение клиента соотв. протоколу JIM и формируем ответ
+            response = process_client_message(msg_to_client)
+            SERVER_LOGGER.debug(f'Cформирован ответ клиенту {response}')
+            send_msg(client, response)
+            SERVER_LOGGER.info(f'Соединение с клиентом {client_address} закрывается.')
+            client.close()
+        except (ValueError, json.JSONDecodeError):
+            SERVER_LOGGER.error(f'Не удалось декодировать JSON строку, полученную от '
+                                f'клиента {client_address}. Соединение закрывается.')
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
